ba61499d7b8e4aae30f43b2c3e386c837345edd8
NicXVII <diminich.nicolas@gmail.com>
2026-03-13 15:05:28 +0100

    Add dSYM files and relocation information for main binary
    - Created Info.plist for dSYM containing metadata for debugging.
    - Added DWARF binary for main executable to support debugging.
    - Included relocation information in YAML format for aarch64 architecture.
    - Compiled object file for t_node with necessary debug information.

--- esercitazioni/c-bst-project/src/main.c
+++ esercitazioni/c-bst-project/src/main.c
@@ -7,8 +7,36 @@
 void s_test(int n);
 void r_test(int n);
 
+void s_test(int n) {
+    int i = 0;
+    bst t = make_bst();
+    printf("Aggiunta dei nodi\n");
+    for (; i < n; i++) {
+        t_node node = make_t_node();
+        node->key = i;
+        bst_insert(t, node);
+    }
+    printf("L'albero e' profondo %d\n", bst_depth(t));
+    delete_bst(t);
+}
+
+void r_test(int n) {
+    int i = 0;
+    bst t = make_bst();
+    printf("Aggiunta dei nodi\n");
+    for (; i < n; i++) {
+        t_node node = make_t_node();
+        node->key = rand();
+        bst_insert(t, node);
+    }
+    printf("L'albero e' profondo %d\n", bst_depth(t));
+    delete_bst(t);
+}
+
 int main(int argc, char *argv[]) {
     int n;
+    (void)argc;
+    (void)argv;
     srand((int)time(NULL));
     printf("Numero di nodi: ");
     while (!scanf("%d", &n));
